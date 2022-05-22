51a8af3efc3c66757197fd3a5c34be0fbafd31eb
MiIiNGeoL <rlaalsrjf6@naver.com>
2022-05-22 23:27:37 +0900

    PlayGame함수 구현, Player클래스 수정

--- object.h
+++ object.h
@@ -41,7 +41,7 @@ private:
     int where;  //0이면 왼쪽, 1이면 오른쪽에 위치
     int bullet; // 가지고 있는 총알의 개수
 public:
-    Player(int x, int y) : Object(x, y), HP(20) { 
+    Player(int x, int y) : Object(x, y), HP(20), bullet(5) { 
         if(x > 60) where = 1;
         else if(x < 30) where = 0;
         else {
@@ -49,13 +49,21 @@ public:
             exit(0);
         }
     }
-    Player(vector<int> vv) : Object(v), HP(20) { 
+    Player(vector<int> vv) : Object(v), HP(20), bullet(5) { 
         if(vv[0] > 60) where = 1;
         else if(vv[0] < 30) where = 0;
         else {
             cout << "Player x is out of range!" << endl;
             exit(0);
         }
+    }
+
+    int GetB() {
+        return bullet;
+    }
+
+    int GetHP() {
+        return HP;
     }
 
     void move(int n) { // n이 0이면 왼쪽으로 이동, 1이면 오른쪽으로 이동
@@ -90,18 +98,14 @@ public:
         HP += n;
     }
 
-    bool isHit(Bomb** arr, int bombcnt) {       // Bomb*를 가지고 있는 배열과 그 배열의 원소의 개수를 인자로 받음
-        for(int i = 0; i < bombcnt; i++) {      // 탄환과 플레이어의 좌표가 같은지 확인 하는 함수. 같으면 탄에 맞은 것이므로 true를 반환하고 아니면 false 반환
-            if(this->GetX() == arr[i]->GetX() && this->GetY() == arr[i]->GetY()) return true;
-        }
-        return false;
+    Bomb* throwbomb() {           // 탄환을 만들어 반환, 플레이어의 x좌표와 y좌표+1에서 생성
+        Bomb* p = new Bomb(*this);
+        return p;
     }
 
-    /*Bomb* throwbomb() {           // 탄환을 만들어 반환, 플레이어의 x좌표와 y좌표+1에서 생성
-        Bomb* p = new Bomb(this->GetX(), this->GetY()+1);
-        return p;
-    }*/
-    
+    void changeBullet(int n) {
+        bullet += n;
+    }
 };
 
 class Bomb : public Object {
