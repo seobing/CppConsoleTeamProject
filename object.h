0676ffee9e3950e4f8b66d2cf01c6ed6d5eafe93
seobing <103929683+seobing@users.noreply.github.com>
2022-05-23 18:47:51 +0900

    object player 제거

--- object.h
+++ object.h
@@ -35,50 +35,5 @@ public:
     }
 };
 
-class Player : public Object {
-private:
-    int HP;
-    int where;  //0이면 왼쪽, 1이면 오른쪽에 위치
-public:
-    Player(int x, int y) : Object(x, y), HP(0) { 
-        if(x > 60) where = 1;
-        else if(x < 30) where = 0;
-        else {
-            cout << "Player x is out of range!" << endl;
-            exit(0);
-        }
-    }
-    Player(vector<int> vv) : Object(v), HP(0) { 
-        if(vv[0] > 60) where = 1;
-        else if(vv[0] < 30) where = 0;
-        else {
-            cout << "Player x is out of range!" << endl;
-            exit(0);
-        }
-    }
-
-    void move(int n) { // n이 0이면 왼쪽으로 이동, 1이면 오른쪽으로 이동
-        if(where == 0) {
-            if(n == 0) {
-                if(v[0] - 1 == -1) return;
-                v[0] -= 1;
-            }
-            else if(n == 1) {
-                if(v[0] + 1 == 40) return;
-                v[0] += 1;
-            }
-        }
-        else if(where == 1) {
-            if(n == 0) {
-                if(v[0] - 1 == 60) return;
-                v[0] -= 1;
-            }
-            else if(n == 1) {
-                if(v[0] + 1 == 100) return;
-                v[0] += 1;
-            }
-        }
-    }
-};
 
 #endif
--- parabola.h
+++ parabola.h
@@ -5,7 +5,7 @@
 int parabola(Bomb* b, int gauge){
     b -> setA(gauge/3); 
     b -> move();
-    b -> go(a*(b -> GetX())*(b -> GetX()), b-> GetY());
+    //b -> go(a*(b -> GetX())*(b -> GetX()), b-> GetY());
 }
 
 #endif
