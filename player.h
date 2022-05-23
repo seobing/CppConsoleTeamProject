95be46804cf73dcf06165cff4ff0db7a30640198
seobing <103929683+seobing@users.noreply.github.com>
2022-05-23 15:25:37 +0900

    player bomb 파일분할

---
+++ player.h
@@ -0,0 +1,77 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+#include "object.h"
+#include "bomb.h"
+class Player : public Object {
+private:
+    int HP;
+    int where;  //0이면 왼쪽, 1이면 오른쪽에 위치
+    int bullet; // 가지고 있는 총알의 개수
+public:
+    Player(int x, int y) : Object(x, y), HP(20), bullet(5) { 
+        if(x > 60) where = 1;
+        else if(x < 30) where = 0;
+        else {
+            cout << "Player x is out of range!" << endl;
+            exit(0);
+        }
+    }
+    Player(vector<int> vv) : Object(v), HP(20), bullet(5) { 
+        if(vv[0] > 60) where = 1;
+        else if(vv[0] < 30) where = 0;
+        else {
+            cout << "Player x is out of range!" << endl;
+            exit(0);
+        }
+    }
+
+    int GetB() {
+        return bullet;
+    }
+
+    int GetHP() {
+        return HP;
+    }
+
+    void move(int n) { // n이 0이면 왼쪽으로 이동, 1이면 오른쪽으로 이동
+        if(where == 0) {
+            if(n == 0) {
+                if(v[0] - 1 == -1) return;
+                v[0] -= 1;
+            }
+            else if(n == 1) {
+                if(v[0] + 1 == 40) return;
+                v[0] += 1;
+            }
+        }
+        else if(where == 1) {
+            if(n == 0) {
+                if(v[0] - 1 == 60) return;
+                v[0] -= 1;
+            }
+            else if(n == 1) {
+                if(v[0] + 1 == 100) return;
+                v[0] += 1;
+            }
+        }
+    }
+
+    bool isAlive() {
+        if(HP > 0) return true;
+        else return false;
+    }
+
+    void changeHP(int n) {
+        HP += n;
+    }
+
+    Bomb* throwbomb() {           // 탄환을 만들어 반환, 플레이어의 x좌표와 y좌표+1에서 생성
+        Bomb* p = new Bomb(*this);
+        return p;
+    }
+
+    void changeBullet(int n) {
+        bullet += n;
+    }
+};
+#endif
