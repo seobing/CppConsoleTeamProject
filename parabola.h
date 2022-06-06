503aba1d0558ebafeb4f12b8697e9638af8e2d08
seobing <103929683+seobing@users.noreply.github.com>
2022-06-06 19:05:55 +0900

    parabola함수 수정

--- parabola.h
+++ parabola.h
@@ -1,11 +1,22 @@
 #ifndef __PARABOLA_H__
 #define __PARABOLA_H__
 #include "derived.h"
+#include<cmath>
 
-int parabola(Bomb* b, int gauge){
-    b -> setA(gauge/3); 
-    b -> move();
-    //b -> go(a*(b -> GetX())*(b -> GetX()), b-> GetY());
+// y = (gauge/3)*(x2-x1)^2 + 10 
+
+int parabola(Bomb* b, double gauge){
+                                                    double a = 10.0/9;
+    b -> setA(5.0/450);
+    //cout << a << endl;
+    //cout << b -> GetX() << endl;
+    cout << b -> getA() << endl; 
+    //b -> move();
+    int bottom = b-> GetX();
+    //double height = (b->getA())*pow(bottom,2) - 100*(b->getA())*bottom + 27.7;
+    double height = (b->getA())*pow(2*bottom - 130, 2);
+    if(height < 0) height = 0;
+    b -> go(bottom, height);
 }
-
+         
 #endif
